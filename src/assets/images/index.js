a6d88a5ccdb902f16abe5e8d322099bddc436357
novalsh <muhammad.harjana@binus.ac.id>
2023-07-16 02:50:45 +0700

    membuat splash screen done

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 import Logo from './logo.png'
+import Background from './background.png'
 
-export {Logo}
+export {Logo, Background}
--- src/pages/splash/index.js
+++ src/pages/splash/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {ImageBackground, Image, StyleSheet, Text, View} from 'react-native';
+import {Logo, Background} from '../../assets';
 
-function splash() {
+const splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('MainApp');
+    }, 3000);
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>Splash</Text>
-    </View>
+    <ImageBackground source={Background} style={styles.background}>
+      <Image source={Logo} style={styles.logo}></Image>
+    </ImageBackground>
   );
-}
+};
 
 export default splash;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    flex: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
